8732f8ca780cc2e85f0c1c8f62fd4d3eb9a22a1e
Ryan Patrick <rmoneymail5@gmail.com>
2021-05-07 21:28:46 -0700

    Serve files to browser.

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function (req, res) {
+    console.log('Responding to a request.');
+    var url = req.url;
+
+    var fileName = 'index.html';
+    if (url.length > 1) {
+        fileName = url.substring(1);
+    }
+    console.log(fileName);
+    var filePath = path.resolve(__dirname, 'app', fileName);
+    console.log(filePath);
+    fs.readFile(filePath, function (err, data) {
+        res.end(data);
+    });
+});
+server.listen(3000);
